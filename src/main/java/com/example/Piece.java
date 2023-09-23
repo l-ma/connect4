6ab52f118b0441930766fd6ff5e493dcea957a89
Lara Marinov <lmarinov@andrew.cmu.edu>
2023-09-22 20:31:38 -0400

    Update Spot and Piece, add basic documentation

--- src/main/java/com/example/Piece.java
+++ src/main/java/com/example/Piece.java
@@ -1,7 +1,18 @@
 package com.example;
 
 public enum Piece {
+    /**
+     * A red piece
+     */
     RED,
+
+    /**
+     * A yellow piece
+     */
     YELLOW,
+
+    /**
+     * A blank piece which represents no piece
+     */
     CLEAR
 }
--- src/main/java/com/example/Spot.java
+++ src/main/java/com/example/Spot.java
@@ -6,6 +6,12 @@ public class Spot {
     private boolean isEmpty;
     private Piece pieceType;
 
+    /**
+     * Creates new instance of a Spot. When created, a spot does not have a piece in it.
+     *
+     * @param x the x-coordinate of the new spot
+     * @param y the y-coordinate of the new spot
+     */
     public Spot(int x, int y) {
         xCoord = x;
         yCoord = y;
@@ -13,14 +19,52 @@ public class Spot {
         pieceType = Piece.CLEAR;
     }
 
-    public boolean isSpotEmpty() {
+    /**
+     * Updates this spot when a piece is dropped into it.
+     *
+     * @param piece The type of piece dropped in
+     * @return true is the update succeeded, false otherwise
+     */
+    public boolean updateSpot(Piece piece) {
+        if (pieceType != Piece.CLEAR) {
+            return false;
+        }
+        pieceType = piece;
+        return true;
+    }
+
+    /**
+     * Checks if this spot is empty.
+     *
+     * @return true is the spot does not have a piece in it
+     */
+    public boolean isEmpty() {
         return isEmpty;
     }
 
-    public Spot getCoordinates() {
-        return new Spot(xCoord, yCoord);
+    /**
+     * Gets the x-coordinate of this spot
+     *
+     * @return x-coordinate
+     */
+    public int getXCoordinate() {
+        return xCoord;
     }
 
+    /**
+     * Gets the y-coordinate of this spot
+     *
+     * @return y-coordinate
+     */
+    public int getYCoordinate() {
+        return yCoord;
+    }
+
+    /**
+     * Gets the type of piece in this spot (e.g. yellow, red, clear)
+     *
+     * @return the type of piece
+     */
     public Piece getPieceType() {
         return pieceType;
     }
