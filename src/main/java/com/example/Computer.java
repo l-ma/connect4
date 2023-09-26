73aa3ff43f6c86b99acb31480026fbb1d7e08813
Lara Marinov <lmarinov@andrew.cmu.edu>
2023-09-26 17:44:37 -0400

    Fix minor style things

--- src/main/java/com/example/Computer.java
+++ src/main/java/com/example/Computer.java
@@ -1,8 +1,9 @@
 package com.example;
 
 import java.util.Random;
-public class Computer extends Player{
-    public int[] dropChecker(){
+
+public class Computer extends Player {
+    public int[] dropChecker() {
         Random random = new Random();
         int row = random.nextInt(20);
         int column = random.nextInt(20);
